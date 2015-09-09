9752c5ee88d32788ef9d00f86a7b87a8a889951a
Joe Giunti <jg4229@gmail.com>
2015-09-08 23:08:50 -0400

    visited
    map to track visited pages and remove them from links to be visited

--- src/degrees/to/cage/DegreesToCage.java
+++ src/degrees/to/cage/DegreesToCage.java
@@ -28,8 +28,8 @@ public class DegreesToCage {
      * @param args the command line arguments
      */
     Cleaner scrubber;
-    HashMap<String, String> upcoming = new HashMap<>();
-    HashMap<String, String> visited = new HashMap<>();
+    HashMap<String, String> upcoming;
+    HashMap<String, String> visited; 
     String searchTerm;
     boolean found;
     Node<String> foundNode = null;
@@ -52,7 +52,8 @@ public class DegreesToCage {
     }
     
     public DegreesToCage(String URL, String search) throws IOException{
-        scrubber = new Cleaner(Whitelist.basicWithImages());  
+        scrubber = new Cleaner(Whitelist.basicWithImages());
+        visited = new HashMap<>();
         Document page = getPage(URL);
         Element starter = new Element(Tag.valueOf("a"), URL);
         linklvl = getLinks(page, starter);
@@ -60,6 +61,7 @@ public class DegreesToCage {
         stack = new ArrayDeque<>();
         nextStack = new ArrayDeque<>();
         stack.push(linklvl);
+       
     }
     
     public Boolean checkHeading(Node<String> node) throws IOException{
@@ -99,20 +101,26 @@ public class DegreesToCage {
         visited.put(node.data, "");
     }
     
-    public static Document getPage(String url) throws IOException{        
+    public Document getPage(String url) throws IOException{        
         Document page = Jsoup.connect(url).timeout(10*1000).get();
         page.setBaseUri(url);
+        visited.put(url, "");
         return page;                            
     }
     
-    public static Elements getLinks(Document page, Element parentElement){
+    public Elements getLinks(Document page, Element parentElement){
         Elements content = page.select("[id=mw-content-text]");
         Elements links = content.select("a[href^=/wiki]:not(a[href^=/wiki/File])"
                 + ":not(a[href^=/wiki/Wikipedia]):not(a[href^=/wiki/Help]):not(a[href^=/wiki/Talk])"
                 + ":not(a[href^=/wiki/Portal]):not(a[href^=/wiki/Special:]");
         for (Element child : links) {
             parentElement.appendChild(child);
-            if(child.attr("abs:href").equals("https://en.wikipedia.org/wiki/Casino")){
+            String childURL = child.attr("href");
+            if (visited.containsKey(child.attr("abs:href"))){
+                    links = links.not("a[href^=" + childURL + "]");
+            }
+            if(childURL.equals("https://en.wikipedia.org/wiki/The_Wizard_of_Oz_(1939_film)")){
+                
                 StringBuilder sb = new StringBuilder();
                 sb.append(child.baseUri());
                 Element temp = child.parent();
