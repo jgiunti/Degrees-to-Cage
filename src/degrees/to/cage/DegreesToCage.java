d1ca02b7507b045e80ae906c7d2f8b5918cf702b
Joe <jgiunti1@iona.edu>
2015-09-11 00:12:04 -0400

    Update DegreesToCage.java

--- src/degrees/to/cage/DegreesToCage.java
+++ src/degrees/to/cage/DegreesToCage.java
@@ -6,11 +6,9 @@
 package degreestocage;
 
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
+import java.util.AbstractQueue;
 import java.util.HashMap;
-import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -28,130 +26,58 @@ public class DegreesToCage {
      * @param args the command line arguments
      */
     Cleaner scrubber;
-    HashMap<String, String> upcoming;
     HashMap<String, String> visited; 
-    String searchTerm;
     boolean found;
     Node<String> foundNode = null;
     Document page;
-    Elements linklvl;
-    Deque<Node<String>> stack;
-    List<Deque<Node<String>>> nextList;
-    List<Deque<Node<String>>> tempList;
-    
-    public void driver() throws IOException{	
-        nextList = tempList;
-        while (!nextList.isEmpty()){
-            stack = nextList.remove(0);
-            while(stack.peek() != null){
-                Node<String> thisElement = stack.pop();               
-                page = getPage(thisElement.data);
-                getLinks(page, thisElement);               
-            }          
-        }
-        
-        driver();
-    }
-    
-    public DegreesToCage(String URL, String search) throws IOException{
-        scrubber = new Cleaner(Whitelist.basicWithImages());
-        searchTerm = search;
-        stack = new ArrayDeque<>();
-        nextList = new ArrayList<>();
-        tempList = new ArrayList<>();
-        visited = new HashMap<>();
-        Document page = getPage(URL);
-        Node<String> rootNode = new Node<>();
-        rootNode.data = URL;
-        getLinks(page, rootNode);
-        
-       
-    }
-    
-    public Boolean checkHeading(Node<String> node) throws IOException{
-
-        String heading = getHeading(page);
-        System.out.println(heading);
-        if(heading.equals(searchTerm)){           
-            foundNode = node;
-            found = true;
-            return true;
-        }
-        else{
-            found = false;
-            return false;
-        }
-    }
-    
-    public void addChildren(Node<String> node) throws IOException{
+    AbstractQueue<Node<String>> queue;
+    int count = 0;
+    int linksChecked = 0;
      
-        /*links.stream().map((link) -> {
-            Node<String> newNode = new Node();
-            newNode.data = link.attr("abs:href");
-            return newNode;
-        }).forEach((newNode) -> {
-            if(!upcoming.containsKey(newNode.data)){
-                if(newNode.data.equalsIgnoreCase("https://en.wikipedia.org/wiki/German_American")){
-                    found = true;
-                    newNode.parent = node;
-                    foundNode = newNode;
-                }
-                newNode.parent = node;
-                newNode.children = new ArrayList<>();
-                node.children.add(newNode);
-                upcoming.put(newNode.getData(), "");
-            }           
-        });*/
-        visited.put(node.data, "");
-    }
     
-    public Document getPage(String url) throws IOException{        
-        Document page = Jsoup.connect(url).timeout(10*1000).get();
-        page.setBaseUri(url);
+    public DegreesToCage() {
+        scrubber = new Cleaner(Whitelist.basicWithImages());
+        queue = new ArrayBlockingQueue<>(200000);
+        visited = new HashMap<>();       
+    }
+      
+    public Document getPage(String url) throws IOException, InterruptedException{ 
+        Thread.sleep(1000); 
+        Document thisPage = Jsoup.connect(url).timeout(10*1000).get();
+        thisPage.setBaseUri(url);
         visited.put(url, "");
-        return page;                            
+        count++;
+        return thisPage;                            
     }
     
-    public void getLinks(Document page, Node<String> parentNode){
-        tempList.clear();
-        Deque<Node<String>> tempStack = new ArrayDeque<>();       
-        Elements content = page.select("[id=mw-content-text]");
-        Elements links = content.select("a[href^=/wiki]:not(a[href^=/wiki/File])"
-                + ":not(a[href^=/wiki/Wikipedia]):not(a[href^=/wiki/Help]):not(a[href^=/wiki/Talk])"
-                + ":not(a[href^=/wiki/Portal]):not(a[href^=/wiki/Special:]):not(a[href^=/wiki/Template:])");
-        for (Element child : links) { //convert these elements to Node<String> to save memory       
-            //System.out.println(child.toString());
-            if (visited.containsKey(child.attr("abs:href"))){
-                    links = links.not("a[href^=" + child.attr("abs:href") + "]");
-                    continue;
+    public Node<String> getLinks(Node<String> rootNode) throws IOException, InterruptedException{                
+        queue.add((rootNode));
+        while(queue.peek() != null){
+            Node<String> thisNode = queue.poll();
+            while (visited.containsKey(thisNode.data)){
+                thisNode = queue.poll();
             }
-            Node<String> childLink = new Node<>();           
-            childLink.parent = parentNode;
-            childLink.data = child.attr("abs:href");
-            parentNode.addChild(childLink);
-            tempStack.add(childLink);
-            if(childLink.data.equals("https://en.wikipedia.org/wiki/Sandpaper")){               
-                foundNode = childLink;
-                StringBuilder sb = new StringBuilder();
-                sb.append(childLink.data);
-                Node<String> temp = childLink.parent;
-                while(temp.parent != null){
-                    sb.append(" ");
-                    sb.append(temp.data);
-                    temp = temp.parent;
-                }
-                System.out.println(sb.toString());
-                System.exit(0);
+            page = getPage(thisNode.data);
+            Elements content = page.select("[id=mw-content-text]");
+            Elements links = content.select("a[href^=/wiki]:not(a[href^=/wiki/File])"
+                    + ":not(a[href^=/wiki/Wikipedia]):not(a[href^=/wiki/Help]):not(a[href^=/wiki/Talk])"
+                    + ":not(a[href^=/wiki/Portal]):not(a[href^=/wiki/Special:]):not(a[href^=/wiki/Template:])");       
+            for(Element link : links){           
+                Node<String> childLink = new Node<>();
+                childLink.parent = thisNode;
+                childLink.data = link.attr("abs:href");
+                queue.add(childLink);
+                linksChecked++;
+                System.out.println(childLink.data);
+                if(childLink.data.equalsIgnoreCase("https://en.wikipedia.org/wiki/Alex_Proyas")){
+                    foundNode = childLink;
+                }                                    
             }
-        }
-        tempList.add(tempStack);
-        links.clear();
-    }
-    
-    public static String getHeading(Document page){
-        Element content = page.select("[id=firstHeading]").first();
-        return content.text();
-    }
-    
-    
+            System.out.println(thisNode.data);
+            System.out.println(linksChecked);
+            System.out.println(count);          
+            }
+        return foundNode;
+              
+    }    
 }
