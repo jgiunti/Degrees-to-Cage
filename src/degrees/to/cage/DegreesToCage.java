b278db926229e5b0ca274eb6aaae23256e95a128
Joe <jgiunti1@iona.edu>
2015-09-11 00:39:41 -0400

    Update DegreesToCage.java

--- src/degrees/to/cage/DegreesToCage.java
+++ src/degrees/to/cage/DegreesToCage.java
@@ -37,27 +37,27 @@ public class DegreesToCage {
     
     public DegreesToCage() {
         scrubber = new Cleaner(Whitelist.basicWithImages());
-        queue = new ArrayBlockingQueue<>(200000);
+        queue = new ArrayBlockingQueue<>(1000000);
         visited = new HashMap<>();       
     }
       
-    public Document GetPage(String url) throws IOException, InterruptedException{ 
-        Thread.sleep(1000); 
+    public Document getPage(String url) throws IOException, InterruptedException{ 
+        //Thread.sleep(1000); 
         Document thisPage = Jsoup.connect(url).timeout(10*1000).get();
         thisPage.setBaseUri(url);
         visited.put(url, "");
-        count++;
+        //count++;
         return thisPage;                            
     }
     
-    public Node<String> Search(Node<String> rootNode) throws IOException, InterruptedException{                
+    public Node<String> getLinks(Node<String> rootNode) throws IOException, InterruptedException{                
         queue.add((rootNode));
         while(queue.peek() != null){
             Node<String> thisNode = queue.poll();
             while (visited.containsKey(thisNode.data)){
                 thisNode = queue.poll();
             }
-            page = GetPage(thisNode.data);
+            page = getPage(thisNode.data);
             Elements content = page.select("[id=mw-content-text]");
             Elements links = content.select("a[href^=/wiki]:not(a[href^=/wiki/File])"
                     + ":not(a[href^=/wiki/Wikipedia]):not(a[href^=/wiki/Help]):not(a[href^=/wiki/Talk])"
@@ -67,16 +67,14 @@ public class DegreesToCage {
                 childLink.parent = thisNode;
                 childLink.data = link.attr("abs:href");
                 queue.add(childLink);
-                linksChecked++;
-                System.out.println(childLink.data);
-                if(childLink.data.equalsIgnoreCase("https://en.wikipedia.org/wiki/Alex_Proyas")){
+                //linksChecked++;
+                //System.out.println(childLink.data);
+                if(childLink.data.equalsIgnoreCase("https://en.wikipedia.org/wiki/Australia")){
                     foundNode = childLink;
+                    return foundNode;
                 }                                    
-            }
-            System.out.println(thisNode.data);
-            System.out.println(linksChecked);
-            System.out.println(count);          
-            }
+            }        
+        }
         return foundNode;
               
     }    
